983612cc72cb237134faaf030473252795fd7b21
pritam-t <techworld1773@gmail.com>
2025-07-01 12:07:35 +0530

    Images

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'daily_page.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 20), (){
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(
+              builder:(context)=> DailyPages()
+          )
+      );
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Center(
+          child: Image.asset(
+            'assets/images/logo.png',
+            height: 200,
+            width: 200
+          ),
+        ),
+      ),
+    );
+  }
+}
