3a367abdedfe9d8c3850efebda90898e08ce5fb3
pritam-t <techworld1773@gmail.com>
2025-07-01 12:31:21 +0530

    Done <3

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 20), (){
+    Timer(Duration(seconds: 3), (){
       Navigator.pushReplacement(context,
           MaterialPageRoute(
               builder:(context)=> DailyPages()
